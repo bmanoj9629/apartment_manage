3fb2a07ad7bfa405b169bdec687f4af286341c1d
bmanoj9629 <bmanoj99629@gmail.com>
2026-06-11 09:32:22 +0530

    fix wsgi render entry

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     MYSQL_HOST = os.environ.get("MYSQL_HOST")
     MYSQL_USER = os.environ.get("MYSQL_USER")
     MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
-    MYSQL_DB = os.environ.get("MYSQL_DATABASE")
+    MYSQL_DB = os.environ.get("MYSQL_DB")  # IMPORTANT FIX
 
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
