a9b3f9a51d5705d28db77e16ddaa18a6626297db
bmanoj9629 <bmanoj99629@gmail.com>
2026-06-11 09:05:07 +0530

    fix mysql railway connection config

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'ApartaSmart@2026#SecretKey!XYZ')
-    
-    # XAMPP MySQL defaults
-    MYSQL_HOST     = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER     = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
-    MYSQL_DB       = os.getenv('MYSQL_DB', 'apartment_manage')
-    MYSQL_CURSORCLASS = 'DictCursor'
-    
-    # Upload
-    UPLOAD_FOLDER  = os.path.join(os.path.dirname(__file__), 'static', 'uploads')
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB
+    MYSQL_HOST = os.environ.get("MYSQL_HOST")
+    MYSQL_USER = os.environ.get("MYSQL_USER")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+    MYSQL_DB = os.environ.get("MYSQL_DATABASE")
+
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
